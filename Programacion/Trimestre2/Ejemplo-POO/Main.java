b779ae821eb1a4e265863f9b2d905501c5e0d3dc
Jesus Carrillo Gonzalez <jesuscarrillogonzalez.93@gmail.com>
2023-01-10 12:55:57 +0100

    Commit ejemplo POO

---
+++ Programacion/Trimestre2/Ejemplo-POO/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        Persona persona1 = new Persona(null, null, 0);
+        persona1.setNombre("Jesus");
+        persona1.setDNI("30266219J");
+        persona1.setEdad(29);
+
+        persona1.mostrarDatos();
+    }
+}
---
+++ Programacion/Trimestre2/Ejemplo-POO/Persona.java
@@ -0,0 +1,38 @@
+public class Persona {
+    public String nombre;
+    public String dni;
+    public int edad;
+
+    //constructor
+    public Persona(String nombre, String dni, int edad){
+        this.nombre = nombre;
+        this.dni = dni;
+        this.edad = edad;
+    }
+
+    //set
+    public void setNombre(String nombre){
+        this.nombre = nombre;
+    }
+    public void setDNI(String dni){
+        this.dni = dni;
+    }
+    public void setEdad(int edad){
+        this.edad = edad;
+    }
+
+    //get
+    public String getNombre(){
+        return nombre;
+    }
+    public String getDNI(){
+        return dni;
+    }
+    public int getEdad(){
+        return edad;
+    }
+
+    public void mostrarDatos(){
+        System.out.println("El nombre de la persona es "+nombre+", su DNI es "+dni+" y su edad es "+edad+".");
+    }
+}
