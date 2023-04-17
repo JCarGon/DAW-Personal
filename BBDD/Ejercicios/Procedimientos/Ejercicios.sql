85b7e26a4d0a038c94fff4ac8ee0a301f75ca254
Jesus Carrillo Gonzalez <121667616+JCarGon@users.noreply.github.com>
2023-04-17 18:09:24 +0200

    clase BBDD 17-04

--- BBDD/Ejercicios/Procedimientos/Ejercicios.sql
+++ BBDD/Ejercicios/Procedimientos/Ejercicios.sql
@@ -69,3 +69,128 @@ END $$
 DELIMITER ;
 
 SELECT hipotenusa(5, 4);
+
+/* "Crea una FUNCION que reciba el NIF de una persona con letra incluida y compruebe si es correcto. 
+En caso de que no coincida número y letra, deberá devolver el valor booleano falso, en caso contrario 
+la función devolverá el valor true. La Función tendrá dos parámeros de entrada: número del DNI y letra, 
+y uno de salida: FALSE si no coincida LETRA GENERADA y LETRA PASADA como SEGUNDO PARÁMETRO" */
+DROP FUNCTION IF EXISTS ValidarDNI;
+DELIMITER //
+CREATE FUNCTION ValidarDNI(dni INT, letra CHAR)
+    RETURNS BOOLEAN
+    BEGIN
+        DECLARE resultado BOOLEAN;
+        DECLARE resto INT;
+        DECLARE abecedario CHAR(23);
+        DECLARE letra1 CHAR(1);
+        SET abecedario = 'TRWAGMYFPDXBNJZSQVHLCKE';
+    
+        SET resto = dni%23;
+        SET letra1 = SUBSTR(abecedario, resto+1, 1);
+
+        IF letra1 = letra THEN SET resultado = TRUE;
+        ELSE SET resultado = FALSE;
+        END IF;
+    
+    RETURN resultado;
+END //
+
+DELIMITER ;
+
+SELECT letraDNI(30266219, 'J'); --> Devuelve 1 (true) o 0 (false)
+
+/* Calcular letra de  NIE. Se pasa una letra X(0), Y(1), Z(2) y el número compuesto de 7 cifras. Se pone al comienzo.
+Se divide el número entre 23, nos quedamos con la parte entera y se multiplica por 23.
+Se resta el número inicial menos el resultado de la multiplicación. El resultado es la posición de la letra en la cadena. */
+DROP FUNCTION IF EXISTS calcularNIE;
+DELIMITER //
+CREATE FUNCTION calcularNIE(letra CHAR, nie INT(7))
+    RETURNS CHAR(1)
+    BEGIN
+        DECLARE resto INT;
+        DECLARE nieNuevo INT(8);
+        DECLARE abecedario CHAR(23);
+        DECLARE letraFinal CHAR(1);
+        SET abecedario = 'TRWAGMYFPDXBNJZSQVHLCKE';
+
+        IF letra = 'X' THEN SET nieNuevo = CONCAT(0, nie);
+        ELSEIF letra = 'Y' THEN SET nieNuevo = CONCAT(1, nie);
+        ELSE SET nieNuevo = CONCAT(2, nie);
+        END IF;
+
+        SET resto = nieNuevo%23;
+        SET letraFinal = SUBSTR(abecedario, resto+1, 1);
+
+    RETURN letraFinal;
+END //
+DELIMITER ;
+
+/* Con CASE */
+DROP FUNCTION IF EXISTS calcularNIE;
+DELIMITER //
+CREATE FUNCTION calcularNIE(letra CHAR, nie INT(7))
+    RETURNS CHAR(1)
+    BEGIN
+        DECLARE resto INT;
+        DECLARE nieNuevo INT(8);
+        DECLARE abecedario CHAR(23);
+        DECLARE letraFinal CHAR(1);
+        SET abecedario = 'TRWAGMYFPDXBNJZSQVHLCKE';
+
+        CASE letra
+            WHEN 'X' THEN SET nieNuevo = CONCAT(0, nie);
+            WHEN 'Y' THEN SET nieNuevo = CONCAT(1, nie);
+            WHEN 'Z' THEN SET nieNuevo = CONCAT(2, nie);
+        END CASE;
+
+        SET resto = nieNuevo%23;
+        SET letraFinal = SUBSTR(abecedario, resto+1, 1);
+
+    RETURN letraFinal;
+END //
+DELIMITER ;
+
+SELECT calcularNIE('Y', 9408905); --> X
+SELECT calcularNIE('Y', 4527393); --> H
+
+/* Confeccionar un PROCEDIMIENO que nos retorne dos valores aleatorios distintos comprendidos entre 0 y 10.
+Los dos valores deben ser enteros. Usa el REPEAT */
+DROP PROCEDURE IF EXISTS numerosRandom;
+DELIMITER //
+CREATE PROCEDURE numerosRandom()
+BEGIN
+    DECLARE valor1 INT;
+    DECLARE valor2 INT;
+    
+    REPEAT
+        SET valor1 = RAND() * 10;
+        SET valor2 = RAND() * 10;
+    UNTIL valor1 != valor2 
+    END REPEAT;
+
+    SELECT valor1, valor2;
+END //
+DELIMITER ;
+
+call numerosRandom;
+
+/* Crea una FUNCION  que devuelva el factorial de un número pasado por parámetro. Debes usar WHILE */
+DROP PROCEDURE IF EXISTS factorial;
+DELIMITER //
+CREATE FUNCTION factorial(num INT)
+    RETURNS INT
+    BEGIN
+        DECLARE numFactorial INT;
+        DECLARE contador INT;
+        SET numFactorial = 1;
+        SET contador = 2;
+
+        WHILE contador <= num DO
+            SET numFactorial = numFactorial*contador;
+            SET contador = contador+1;
+        END WHILE;
+    RETURN numFactorial;
+    END //
+DELIMITER ;
+
+SELECT factorial(5);
