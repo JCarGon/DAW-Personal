7924ad7c6b5b56d88e2616e973510aae70a8d587
Jesus Carrillo Gonzalez <121667616+JCarGon@users.noreply.github.com>
2023-05-05 12:23:24 +0200

    update TareaTigger-BBDD

--- BBDD/Ejercicios/Trigger/TareaTriger.sql
+++ BBDD/Ejercicios/Trigger/TareaTriger.sql
@@ -1,20 +1,71 @@
-/*Estructura de un trigger donde:
+Crea una  base de datos llamada test que contenga una tabla llamada alumnos con las siguientes columnas.
+Tabla alumnos:
 
-    nombre_trigger: es el nombre que se le da al trigger.
-    BEFORE/AFTER: indica si el trigger se dispara antes o después de la operación que lo activa (insertar, actualizar o eliminar).
-    INSERT/UPDATE/DELETE: indica la acción que dispara el trigger.
-    nombre_tabla: es el nombre de la tabla sobre la cual se aplica el trigger.
-    FOR EACH ROW: indica que el trigger se ejecuta para cada fila afectada por la operación que lo activa.
-    BEGIN: marca el inicio del código del trigger.
-    END: marca el final del código del trigger.
+    id (entero sin signo)
+    nombre (cadena de caracteres)
+    apellido1 (cadena de caracteres)
+    apellido2 (cadena de caracteres)
+    nota (número real)
 
-Dentro del cuerpo del trigger se puede incluir cualquier código SQL válido, como por ejemplo, consultas, operaciones de inserción, actualización o eliminación, entre otras.*/
+CREATE DATABASE test;
+USE test;
+CREATE TABLE IF NOT EXISTS alumnos(
+    id INT(8) UNSIGNED PRIMARY KEY,
+    nombre VARCHAR(15) NOT NULL,
+    apellido1 VARCHAR(15) NOT NULL,
+    apellido2 VARCHAR(15) NOT NULL,
+    nota REAL NOT NULL
+);
 
-CREATE TRIGGER nombre_trigger
-{BEFORE | AFTER} {INSERT | UPDATE | DELETE}
-ON nombre_tabla
+Una vez creada la tabla escriba dos triggers con las siguientes características:
+
+    Trigger 1: trigger_check_nota_before_insert
+    Se ejecuta sobre la tabla alumnos.
+    Se ejecuta  de una operación de inserción  .
+    Si el nuevo valor de la nota que se quiere insertar es negativo, se guarda como 0.
+    Si el nuevo valor de la nota que se quiere insertar es mayor que 10, se guarda como 10.
+
+DROP TRIGGER IF EXISTS trigger_check_nota_before_insert;
+DELIMITER $$
+CREATE TRIGGER trigger_check_nota_before_insert
+BEFORE INSERT
+ON alumnos
 FOR EACH ROW
 BEGIN
-   -- Código del trigger
-END;
+    IF NEW.nota < 0 THEN SET NEW.nota = 0;
+    ELSEIF NEW.nota > 10 THEN SET NEW.nota = 10;
+    END IF;
+END$$
+DELIMITER ;
 
+    Trigger2 : trigger_check_nota_before_update
+    Se ejecuta sobre la tabla alumnos.
+    Se ejecuta  de una operación de actualización .
+    Si el nuevo valor de la nota que se quiere actualizar es negativo, se guarda como 0.
+    Si el nuevo valor de la nota que se quiere actualizar es mayor que 10, se guarda como 10.
+
+DROP TRIGGER IF EXISTS trigger_check_nota_before_update;
+DELIMITER $$
+CREATE TRIGGER trigger_check_nota_before_update
+BEFORE UPDATE
+ON alumnos
+FOR EACH ROW
+BEGIN
+    IF NEW.nota < 0 THEN SET NEW.nota = 0;
+    ELSEIF NEW.nota > 10 THEN SET NEW.nota = 10;
+    END IF;
+END$$
+DELIMITER ;
+
+Una vez creados los triggers escriba varias sentencias de inserción y actualización 
+sobre la tabla alumnos y verifica que los triggers se están ejecutando correctamente.
+
+INSERT INTO ALUMNOS VALUES 
+(1, "Manolín", "García", "Pérez", -3.0),
+(2, "Pepe", "Viyuela", "Muñoz", 14.0),
+(3, "Juanito", "Becerra", "González", 7.5);
+/* La nota de Manolín sería un 0, la de Pepe un 10 y la de Juanito un 7.5 */
+
+UPDATE alumnos SET nota = -4.0 WHERE nombre="Juanito"; /* Le cambia la nota a Juanito, pasando de un 7.5 a 0 */
+UPDATE alumnos SET nota = 11 WHERE nombre="Manolín"; /* Le cambia la nota a Manolín, pasando de un 0 a un 10 */
+UPDATE alumnos SET nota = 6.8 WHERE nombre="Pepe"; /* Le cambia la nota a Pepe, pasando de un 10 a un 6.8 */
