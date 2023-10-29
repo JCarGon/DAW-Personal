38d3f97e14c2573ad9702a9401a4a5bc1fef8ded
Jesús <jesuscarrillogonzalez.93@gmail.com>
2023-10-29 20:07:32 +0100

    continue index.html & css

--- Segundo/Proyecto/index.js
+++ Segundo/Proyecto/index.js
@@ -16,3 +16,15 @@ window.addEventListener('scroll', function() {
       navBg.style.top = 'auto';
   }
 });
+
+let line1__bars = document.querySelector(".line1__bars-menu");
+let line2__bars = document.querySelector(".line2__bars-menu");
+let line3__bars = document.querySelector(".line3__bars-menu");
+
+function animateBars() {
+  line1__bars.classList.toggle("activeline1__bars-menu");
+  line2__bars.classList.toggle("activeline2__bars-menu");
+  line3__bars.classList.toggle("activeline3__bars-menu");
+}
+
+document.querySelector(".bars__menu").addEventListener("click", animateBars);
