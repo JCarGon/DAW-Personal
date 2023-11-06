b42e28695133e7bc618f0b52f8df943e0b78461b
Jesus Carrillo Gonzalez <121667616+JCarGon@users.noreply.github.com>
2023-11-06 20:50:51 +0100

    act boilerplate express

--- Segundo/Entorno Servidor/Boilerplate/src/index.js
+++ Segundo/Entorno Servidor/Boilerplate/src/index.js
@@ -1,11 +1,23 @@
-/* eslint-disable no-console */
-import http from 'http';
+/* eslint-disable import/extensions */
+// eslint-disable-next-line import/no-extraneous-dependencies
+import express from 'express';
+import { routes, defaultRoute } from './server/routes.js';
 
-const server = http.createServer(() => {
-  console.log('Server created');
-});
+const port = 3000;
 
-// Configure server
-server.listen(3000, () => {
-  console.log('Server is ready to take request');
+const server = express();
+
+server.get('/ping', routes.ping);
+server.get('/json', routes.json);
+server.get('/page', routes.page);
+server.get('/error', routes.error);
+server.get('/cat', routes.cat);
+server.get('/hello', routes.hello);
+server.get('/pageHTML', server.use('/pageHTML', express.static('./public/page.html')));
+server.get('/fizzBuzz', routes.fizzbuzz);
+server.get('*', defaultRoute);
+
+server.listen(port, () => {
+  // eslint-disable-next-line no-console
+  console.log(`Server ready at port: ${port}`);
 });
