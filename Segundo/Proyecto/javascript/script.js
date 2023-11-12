4be70a5b2360c8574c230cf67e827fa8e41b93b1
Jesús <jesuscarrillogonzalez.93@gmail.com>
2023-11-12 16:27:49 +0100

    continue landingPage and products (project)

--- Segundo/Proyecto/javascript/script.js
+++ Segundo/Proyecto/javascript/script.js
@@ -31,3 +31,35 @@ function hideMenu(){
   nav.style.right = "-330px";
   background_menu.style.display = "none";
 }
+
+document.addEventListener("DOMContentLoaded", function() {
+  // Espera a que el DOM esté completamente cargado
+
+  // Selecciona el botón "Cargar más" por su ID
+  const loadMoreButton = document.getElementById("loadMoreButton");
+
+  // Agrega un evento de clic al botón
+  loadMoreButton.addEventListener("click", function() {
+    // Crea un nuevo elemento div con la clase "product-row"
+    const newRow = document.createElement("div");
+    newRow.className = "product-row";
+
+    // Agrega dos productos a la nueva fila (puedes personalizar esto según tus necesidades)
+    newRow.innerHTML = `
+      <div class="product-image">
+        <img class="product-promo" src="imagenes/Kimetsu/Kimetsu1.png" alt="Nuevo Producto 1" width="500px">
+        <p class="product-title">Nuevo Producto 1</p>
+        <p class="product-price">PRECIO: 100€ <del><small>(PVP: 120€)</small></del></p>
+      </div>
+      <div class="product-image">
+        <img class="product-promo" src="imagenes/Kimetsu/Kimetsu4.png" alt="Nuevo Producto 2" width="500px">
+        <p class="product-title">Nuevo Producto 2</p>
+        <p class="product-price">PRECIO: 150€ <del><small>(PVP: 180€)</small></del></p>
+      </div>
+    `;
+
+    // Selecciona el contenedor de productos existente y agrega la nueva fila al final
+    const productContainer = document.querySelector(".product-container");
+    productContainer.appendChild(newRow);
+  });
+});
