9efe6fb0f641631260416a59452ba3ca592a6d59
Jesus Carrillo Gonzalez <121667616+JCarGon@users.noreply.github.com>
2023-12-12 20:45:24 +0100

    start swagger notes

--- Segundo/Entorno Servidor/Trimestre1/proyecto-notas/src/loaders/express-loader.js
+++ Segundo/Entorno Servidor/Trimestre1/proyecto-notas/src/loaders/express-loader.js
@@ -1,15 +1,26 @@
+/* eslint-disable import/no-extraneous-dependencies */
+import { fileURLToPath } from 'url';
+import path from 'path';
 import express from 'express';
-// eslint-disable-next-line import/no-extraneous-dependencies
 import cors from 'cors';
+import swaggerUi from 'swagger-ui-express';
+import YAML from 'yamljs';
 
 import router from '../routes/index.js';
 import errorMiddleware from '../middlewares/error-middleware.js';
 import printDateMiddleware from '../middlewares/misc-middleware.js';
 
+const filename = fileURLToPath(import.meta.url);
+const dirname = path.dirname(filename);
+
+const swaggerDocument = YAML.load(path.join(dirname, '../openapi/openapi.yml'));
+
 export default function expressLoader(server) {
   server.use(cors());
   server.use(express.json());
   server.use(express.urlencoded());
+  /* Swagger */
+  server.use('/swagger', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
   server.use(printDateMiddleware);
 
