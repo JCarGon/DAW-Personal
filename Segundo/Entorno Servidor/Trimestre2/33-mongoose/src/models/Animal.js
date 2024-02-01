a77dd3c2ac6eb41b2df5970eedef8aaaa329df7c
Jesus Carrillo Gonzalez <121667616+JCarGon@users.noreply.github.com>
2024-02-01 18:59:39 +0100

    changes in animal schema

--- Segundo/Entorno Servidor/Trimestre2/33-mongoose/src/models/Animal.js
+++ Segundo/Entorno Servidor/Trimestre2/33-mongoose/src/models/Animal.js
@@ -3,8 +3,8 @@ import mongoose from "mongoose";
 const { Schema } = mongoose;
 
 const animalSchema = new Schema({
-  name: String,
-  color: String,
+  name: { type: String, required: true, unique: true },
+  color: String, enum: ['rojo', 'azul', 'amarillo'], //color: [String] se convierte en un array colores
   legs: String,
   hasTail: Boolean,
   age: Number,
